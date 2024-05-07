0d866de78aee80a146c4200a7cd038a021013a6d
Morgane55440 <104371839+Morgane55440@users.noreply.github.com>
2024-05-07 19:08:31 +0200

    fixed the fix (tested on machine this time) (don't code in your browser) (#2)

--- src/filter_impl.cpp
+++ src/filter_impl.cpp
@@ -16,7 +16,7 @@ extern "C" {
             uint8_t* lineptr = (uint8_t*) (buffer + y * stride);
             for (int x = 0; x < width; ++x)
             {
-                rgb* pxl = (rgb *) (lineptr + x * pixel_stride)
+                rgb pxl = *(rgb *) (lineptr + x * pixel_stride);
                 pxl.r = 0; // Back out red component
 
                 if (x < logo_width && y < logo_height)
