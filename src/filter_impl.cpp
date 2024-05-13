5020defc036b8aa7c45eabebddc9212a5a9c3112
Clang Robot <robot@example.com>
2024-05-13 22:37:16 +0000

    Committing clang-format changes

--- src/filter_impl.cpp
+++ src/filter_impl.cpp
@@ -4,35 +4,40 @@
 #include <thread>
 #include "logo.h"
 
-struct rgb {
-    uint8_t r, g, b;
+struct rgb
+{
+  uint8_t r, g, b;
 };
 
-extern "C" {
-    void filter_impl(uint8_t* buffer, int width, int height, int stride, int pixel_stride)
+extern "C"
+{
+  void filter_impl(uint8_t* buffer,
+                   int width,
+                   int height,
+                   int stride,
+                   int pixel_stride)
+  {
+    for (int y = 0; y < height; ++y)
+      {
+        uint8_t* lineptr = (uint8_t*)(buffer + y * stride);
+        for (int x = 0; x < width; ++x)
+          {
+            rgb pxl = *(rgb*)(lineptr + x * pixel_stride);
+            pxl.r = 0; // Back out red component
+
+            if (x < logo_width && y < logo_height)
+              {
+                float alpha = logo_data[y * logo_width + x] / 255.f;
+                pxl.g = uint8_t(alpha * pxl.g + (1 - alpha) * 255);
+                pxl.b = uint8_t(alpha * pxl.b + (1 - alpha) * 255);
+              }
+          }
+      }
+
+    // You can fake a long-time process with sleep
     {
-        for (int y = 0; y < height; ++y)
-        {
-            uint8_t* lineptr = (uint8_t*) (buffer + y * stride);
-            for (int x = 0; x < width; ++x)
-            {
-                rgb pxl = *(rgb *) (lineptr + x * pixel_stride);
-                pxl.r = 0; // Back out red component
-
-                if (x < logo_width && y < logo_height)
-                {
-                    float alpha = logo_data[y * logo_width + x] / 255.f;
-                    pxl.g = uint8_t(alpha * pxl.g + (1-alpha) * 255);
-                    pxl.b = uint8_t(alpha * pxl.b + (1-alpha) * 255);
-
-                }
-            }
-        }
-
-        // You can fake a long-time process with sleep
-        {
-            using namespace std::chrono_literals;
-            //std::this_thread::sleep_for(100ms);
-        }
-    }   
+      using namespace std::chrono_literals;
+      //std::this_thread::sleep_for(100ms);
+    }
+  }
 }
--- src/filter_impl.h
+++ src/filter_impl.h
@@ -1,13 +1,18 @@
 #pragma once
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-void filter_impl(uint8_t* buffer, int width, int height, int plane_stride, int pixel_stride);
+  void filter_impl(uint8_t* buffer,
+                   int width,
+                   int height,
+                   int plane_stride,
+                   int pixel_stride);
 
 #ifdef __cplusplus
 }
