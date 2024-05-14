fca61b61170c3ad71b15510899af91912925bf34
Ahmed Hassayoune <ahmed.hassayoune@epita.fr>
2024-05-14 20:15:59 +0200

    feat: Add hysteresis thresholding algorithm to filter implementation

--- src/filter_impl.h
+++ src/filter_impl.h
@@ -16,4 +16,26 @@ extern "C"
 
 #ifdef __cplusplus
 }
+struct pos
+{
+  int x, y;
+};
+
+struct Queue
+{
+  pos* buffer_pos;
+  int front = 0;
+  int rear = 0;
+};
+
+void enqueue(Queue& q, int x, int y);
+pos dequeue(Queue& q);
+bool is_empty(Queue& q);
+void apply_hysteresis_threshold(uint8_t* buffer,
+                                int width,
+                                int height,
+                                int stride,
+                                int pixel_stride,
+                                uint8_t low_threshold,
+                                uint8_t high_threshold);
 #endif
