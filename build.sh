d5e63aaeb28dc8ee54ae8c87c797d006793bc064
Ahmed Hassayoune <ahmed.hassayoune@epita.fr>
2024-05-27 18:05:03 +0200

    feat: add uri video parameter in build script

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+# Check if a URI parameter is provided, otherwise use default
+URI_FILE=${1:-sintel_trailer-480p.webm}
+
 cmake -S . -B build --preset release -D USE_CUDA=ON  # 1 (ou debug)
 cmake --build build                                  # 1
 
-
-if [ ! -f sintel_trailer-480p.webm ]; then
+# Download the default file if it's not present and if the default is used
+if [ "$URI_FILE" = "sintel_trailer-480p.webm" ] && [ ! -f sintel_trailer-480p.webm ]; then
     wget https://gstreamer.freedesktop.org/media/sintel_trailer-480p.webm
 fi
+
 export GST_PLUGIN_PATH=$(pwd)                                         # 3
 ln -s ./build/libgstcudafilter-cpp.so libgstcudafilter.so             # 4
 
+# Construct the URI path
+URI_PATH="file://$(pwd)/$URI_FILE"
 
-gst-launch-1.0 uridecodebin uri=file://$(pwd)/sintel_trailer-480p.webm ! videoconvert ! "video/x-raw, format=(string)RGB" ! cudafilter ! videoconvert ! video/x-raw, format=I420 ! x264enc ! mp4mux ! filesink location=video.mp4 #4
+gst-launch-1.0 uridecodebin uri=$URI_PATH ! videoconvert ! "video/x-raw, format=(string)RGB" ! cudafilter ! videoconvert ! video/x-raw, format=I420 ! x264enc ! mp4mux ! filesink location=video.mp4 #4
